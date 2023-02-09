26d27d818afe7427408d0759d037522f1781e2d4
Yoann <yoann.deblois@gmail.com>
2023-02-09 23:13:51 +0100

    Debug: changed spider as the website layout changed too (from 19 jobs in a page to 6 in a page).

--- spiders/django_jobs_spider.py
+++ spiders/django_jobs_spider.py
@@ -19,8 +19,20 @@ class DjangojobsSpider(scrapy.Spider):
         'https://djangojobs.net/jobs/?page=3',
         'https://djangojobs.net/jobs/?page=4',
         'https://djangojobs.net/jobs/?page=5',
-        'https://djangojobs.net/jobs/?page=6'
+        'https://djangojobs.net/jobs/?page=6',
+        'https://djangojobs.net/jobs/?page=7',
+        'https://djangojobs.net/jobs/?page=8',
+        'https://djangojobs.net/jobs/?page=9',
+        'https://djangojobs.net/jobs/?page=10',
+        'https://djangojobs.net/jobs/?page=11',
+        'https://djangojobs.net/jobs/?page=12',
+        'https://djangojobs.net/jobs/?page=13',
+        'https://djangojobs.net/jobs/?page=14',
+        'https://djangojobs.net/jobs/?page=15',
+        'https://djangojobs.net/jobs/?page=16',
+        'https://djangojobs.net/jobs/?page=17'
     ]
+
     job_counter = 0
 
     def parse(self, response):
@@ -28,15 +40,17 @@ class DjangojobsSpider(scrapy.Spider):
         link = response.css('h4 a::attr(href)').getall()
         location_pub_date = response.css("div.clearfix div.float-right::text").getall()
         remote_relocation = response.css('div.clearfix::text').getall()
-        if self.job_counter <= 81:
-            jobs_in_page = 19
+        if self.job_counter <= 95:
+            jobs_in_page = 6
         else:
             jobs_in_page = 4
 
-        for i in range(0, jobs_in_page+1):
+        for i in range(0, jobs_in_page):
             location = location_pub_date[i][:-15].split(',')
             if len(location) == 2:
                 location = [location[0], '', [location[1]]]
+            else:
+                location = [location[0], '', '']
             yield {
                 'title': title[i*2].replace(',', ''),
                 'company': title[i*2+1].replace(',', ''),
@@ -51,3 +65,5 @@ class DjangojobsSpider(scrapy.Spider):
                 'relocation': remote_relocation[i*2][-16:-13].strip()
             }
             self.job_counter += 1
+
+        print(f'Total Django_jobs yielded : {self.job_counter}')
